182e443df8afa95a0bc71fbe94effa82cd7e9a87
SuryaSh8055 <suryanshbsharma@gmail.com>
2025-03-08 20:00:14 +0530

    Initial commit - Meal Suggestion App

---
+++ data.js
@@ -0,0 +1,40 @@
+let meals = {
+    Breakfast: [
+        "Sprouts",
+        "Poha",
+        "Uttapam",
+        "Paratha",
+        "Bambino",
+        "Upma",
+        "Bread Butter"
+    ],
+
+    Lunch: [
+        "Arhar Dal + Rice",
+        "Masoor Dal and Rice",
+        "Urad Dal",
+        "Chole + Rice",
+        "Rajma + Rice",
+        "Black Urad",
+        "Kadhi + Rice",
+        "Kadhai Paneer",
+        "Aloo Kofte",
+        "Mangodi",
+        "Lauki Chana Dal",
+        "Samose",
+        "Tikki",
+        "Idli"
+    ],
+
+    Dinner: [
+        "Pumpkin",
+        "Bhindi",
+        "Fried Aloo",
+        "Aloo Paratha",
+        "Tinde",
+        "Lauki",
+        "Karela",
+        "Fali",
+        "Hari Moong"
+    ]
+};
---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById("suggestMeal");
+    let mealDisplay = document.getElementById("mealDisplay");
+    let mealTypeSelect = document.getElementById("MealType");
+    
+    button.addEventListener("click", function() {
+        let selectedType = mealTypeSelect.value;
+
+        if(meals[selectedType] && meals[selectedType].length > 0) {
+            const randomMeal = meals[selectedType][Math.floor(Math.random() * meals[selectedType].length)];
+            mealDisplay.textContent = `How about: ${randomMeal} today?`;
+        } else {
+            mealDisplay.textContent = "No meals available!";
+        }
+    });
+});
