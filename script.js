80be72a82fd646bf7e482b83860a9857e54febbe
Suryansh Sharma <98092453+SuryaSh8055@users.noreply.github.com>
2025-03-08 20:23:28 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if(meals[selectedType] && meals[selectedType].length > 0) {
             const randomMeal = meals[selectedType][Math.floor(Math.random() * meals[selectedType].length)];
-            mealDisplay.textContent = `How about: <span>${randomMeal}</span> today?`;
+            mealDisplay.textContent = `How about: <span>${randomMeal}</span>?`;
         } else {
             mealDisplay.textContent = "No meals available!";
         }
